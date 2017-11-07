aaa6212872c2cde6a158e3abe71a636bd763cd92
Daidson Fonseca Alves <daidson.alves@gmail.com>
2017-11-07 18:23:33 -0300

    ident.c ident.x

--- ident.c
+++ ident.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 5
+
 int a;
 int b;
-int c;
+int c[MAX][MAX];
+char z;
 
+void protPrintNum()
+{
+    for(a=0; a < MAX; a++)
+    {
+        for(b=0; b < MAX; b++)
+        {
+            printf("%d",c[a][b]);
+        }
+        printf("\n");
+        
+    }
+}
 
 int main(void)
 {
-    printf("Bem vindo ao teste de identacao.\n\n");
+    /*printf("Bem vindo ao teste de identacao.\n\n");
     getchar();
 
     printf("Digite dois numeros de 1 a 10.\n\n");
 
     scanf("%d", &a);
     scanf("%d", &b);
+
+    scanf("%s", &z);
 
     if(a>b)
     {
@@ -39,7 +56,8 @@ int main(void)
             c = a;
             printf("O valor de c, nessas condicoes, eh: %d\n\n", c);
         }
-    }
+    }*/
+    protPrintNum();
     return 0;
 }
 
